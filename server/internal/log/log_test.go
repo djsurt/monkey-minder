6c43330a82c8b50cb076bc9afd098c6bc92058d5
Caleb Fringer <cfringer98@gmail.com>
2025-11-25 11:25:35 -0800

    Add Commit and commitIdx to keep track of appended log entries that have not been committed.

--- server/internal/log/log_test.go
+++ server/internal/log/log_test.go
@@ -33,28 +33,58 @@ func TestLog(t *testing.T) {
 	assertEq(t, log.IndexAfterLast(), Index(1))
 
 	log.Append(simpleEntry{"a", 1})
-	assertEq(t, (*log.Latest())["a"], 1)
 	assertEq(t, log.LenLogical(), 1)
 	assertEq(t, log.IndexBeforeFirst(), Index(0))
 	assertEq(t, log.IndexAfterLast(), Index(2))
 
 	log.Append(simpleEntry{"a", 2})
-	assertEq(t, (*log.Latest())["a"], 2)
 	assertEq(t, log.LenLogical(), 2)
 	assertEq(t, log.IndexBeforeFirst(), Index(0))
 	assertEq(t, log.IndexAfterLast(), Index(3))
 
 	log.Append(simpleEntry{"b", 3})
-	assertEq(t, (*log.Latest())["a"], 2)
-	assertEq(t, (*log.Latest())["b"], 3)
 	assertEq(t, log.LenLogical(), 3)
 	assertEq(t, log.IndexBeforeFirst(), Index(0))
 	assertEq(t, log.IndexAfterLast(), Index(4))
 
 	log.SquashFirstN(1)
-	assertEq(t, (*log.Latest())["a"], 2)
 	assertEq(t, log.LenLogical(), 3)
 	assertEq(t, log.LenActual(), 2)
 	assertEq(t, log.IndexBeforeFirst(), Index(1))
 	assertEq(t, log.IndexAfterLast(), Index(4))
+}
+
+func TestCommit(t *testing.T) {
+	log := NewLog(make(simpleSnapshot), 0)
+	assertEq(t, log.IndexBeforeFirst(), Index(0))
+	assertEq(t, log.IndexAfterLast(), Index(1))
+	assertEq(t, log.GetCommitIndex(), Index(0))
+
+	log.Append(simpleEntry{"a", 1})
+	assertEq(t, (*log.Latest())["a"], 0)
+	assertEq(t, log.IndexOfLast(), Index(1))
+	assertEq(t, log.GetCommitIndex(), Index(0))
+
+	log.Commit(log.IndexOfLast())
+	assertEq(t, log.GetCommitIndex(), Index(1))
+	assertEq(t, (*log.Latest())["a"], 1)
+
+	log.Append(simpleEntry{"b", 2})
+	log.Append(simpleEntry{"c", 3})
+	log.Append(simpleEntry{"d", 4})
+
+	assertEq(t, log.IndexOfLast(), Index(4))
+	assertEq(t, log.GetCommitIndex(), Index(1))
+
+	log.Commit(Index(3))
+	assertEq(t, log.IndexOfLast(), Index(4))
+	assertEq(t, log.GetCommitIndex(), Index(3))
+	assertEq(t, (*log.Latest())["c"], 3)
+
+	log.Commit(Index(4))
+	assertEq(t, log.GetCommitIndex(), log.IndexOfLast())
+	// Commits should be idempotent
+	log.Commit(Index(4))
+	assertEq(t, log.GetCommitIndex(), log.IndexOfLast())
+	assertEq(t, (*log.Latest())["d"], 4)
 }
