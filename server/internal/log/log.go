71f56f3f6eadca3c05ceff0e70425b306b394b54
amgg <2adrian2@gmail.com>
2025-11-24 23:11:53 -0800

    log checkpoints + other log changes
    - add log checkpoint api
    - add log `HasEntryAt(index)` helper method
    - fix log `Apply` method & change its signature: we were ignoring errors before

--- server/internal/log/log.go
+++ server/internal/log/log.go
@@ -2,6 +2,7 @@ package log
 
 import (
 	"errors"
+	"fmt"
 	"slices"
 )
 
@@ -10,11 +11,27 @@ type Snapshot[Entry any, Self any] interface {
 	Clone() Self
 }
 
+type checkpointId uint64
+
+type checkpointData[E any, S Snapshot[E, S]] struct {
+	index    Index
+	snapshot S
+	// lowest index which we have not yet applied to this checkpoint's snapshot
+	firstUnapplied Index
+}
+
+type Checkpoint[E any, S Snapshot[E, S]] struct {
+	log *Log[E, S]
+	id  checkpointId
+}
+
 type Log[E any, S Snapshot[E, S]] struct {
 	headSnapshot   S
 	tailSnapshot   S
 	realFirstIndex nonzeroIndex
 	entries        []E
+	nextCheckpoint checkpointId
+	checkpoints    map[checkpointId]*checkpointData[E, S]
 }
 
 func NewLog[E any, S Snapshot[E, S]](
@@ -26,6 +43,8 @@ func NewLog[E any, S Snapshot[E, S]](
 		tailSnapshot:   initialSnapshot,
 		realFirstIndex: Index(1).promiseNonzero().offsetBy(indexOffset),
 		entries:        []E{},
+		nextCheckpoint: checkpointId(0),
+		checkpoints:    make(map[checkpointId]*checkpointData[E, S]),
 	}
 }
 
@@ -60,9 +79,17 @@ func (log *Log[Entry, _]) SquashUntil(predicate func(Entry) bool) (err error) {
 }
 
 // append a new entry to the log
-func (log *Log[E, S]) Append(entry E) {
-	log.tailSnapshot.ApplyEntry(entry)
+func (log *Log[E, S]) Append(entry E) (err error) {
+	err = log.tailSnapshot.ApplyEntry(entry)
+	if err != nil {
+		return
+	}
 	log.entries = append(log.entries, entry)
+	err = log.updateCheckpoints()
+	if err != nil {
+		return
+	}
+	return
 }
 
 // a snapshot at the current state of the log.
@@ -102,13 +129,17 @@ func (log *Log[E, S]) IndexOfLast() Index {
 	return log.indexAfterLast().prior()
 }
 
+func (log *Log[E, S]) getEntryAtUnchecked(index Index) (entry *E) {
+	return &log.entries[index-log.realFirstIndex.unwrap()]
+}
+
 func (log *Log[E, S]) GetEntryAt(index Index) (entry *E, err error) {
 	if index < log.realFirstIndex.unwrap() {
 		return nil, errors.New("provided index lies below first actual entry of log")
 	} else if index > log.IndexOfLast() {
 		return nil, errors.New("provided index lies after last entry of log")
 	} else {
-		return &log.entries[index-log.realFirstIndex.unwrap()], nil
+		return log.getEntryAtUnchecked(index), nil
 	}
 }
 
@@ -123,4 +154,97 @@ func (log *Log[E, S]) GetEntryLatest() (entryMaybe *E, idx Index) {
 	} else {
 		return nil, log.realFirstIndex.prior()
 	}
+}
+
+// returns true iif the provided index is the index of a current entry in the log
+func (log *Log[E, S]) HasEntryAt(index Index) bool {
+	return log.IndexBeforeFirst() < index && index < log.IndexAfterLast()
+}
+
+func (ckpt *checkpointData[E, S]) updateSnapshot(log *Log[E, S]) error {
+	for ckpt.firstUnapplied <= ckpt.index && log.HasEntryAt(ckpt.firstUnapplied) {
+		applyErr := ckpt.snapshot.ApplyEntry(*log.getEntryAtUnchecked(ckpt.firstUnapplied))
+		if applyErr != nil {
+			return fmt.Errorf("error encountered while applying log entry at index %v: %w", ckpt.firstUnapplied, applyErr)
+		}
+		ckpt.firstUnapplied++
+	}
+	return nil
+}
+
+func (log *Log[E, S]) updateCheckpoints() error {
+	for id, ckpt := range log.checkpoints {
+		err := ckpt.updateSnapshot(log)
+		return fmt.Errorf("error while updating checkpoint #%v: %w", id, err)
+	}
+	return nil
+}
+
+func (log *Log[E, S]) NewCheckpointAt(index Index) (*Checkpoint[E, S], error) {
+	if !(log.HasEntryAt(index) || index == log.IndexBeforeFirst()) {
+		return nil, errors.New("provided index must be the index of a valid element, or the index immediately before the first element, or the index immediately after the last element")
+	}
+
+	ckpt := checkpointData[E, S]{
+		index:          index,
+		snapshot:       log.headSnapshot.Clone(),
+		firstUnapplied: log.IndexBeforeFirst() + 1,
+	}
+	err := ckpt.updateSnapshot(log)
+	if err != nil {
+		return nil, fmt.Errorf("error encountered during initial fast-forward of a new log checkpoint with target index %v: %w", index, err)
+	}
+
+	id := log.nextCheckpoint
+	log.nextCheckpoint++
+	log.checkpoints[id] = &ckpt
+
+	check := Checkpoint[E, S]{
+		log: log,
+		id:  id,
+	}
+	return &check, nil
+}
+
+func (check Checkpoint[E, S]) data() *checkpointData[E, S] {
+	ckpt, ok := check.log.checkpoints[check.id]
+	if !ok {
+		panic("Checkpoint handle #%v has no corresponding data")
+	}
+	return ckpt
+}
+
+// the index this checkpoint is logically pointing at.
+//
+// this is usually also the index of a corresponding entry, unless the checkpoint
+// is currently pointing at the space immediately before the start of the log or
+// immediately after the end of the log.
+func (check Checkpoint[E, S]) Index() Index {
+	return check.data().index
+}
+
+// get the snapshot at the checkpoint
+func (check Checkpoint[E, S]) Data() *S {
+	return &check.data().snapshot
+}
+
+func (check Checkpoint[E, S]) AdvanceBy(amount uint64) error {
+	ckpt := check.data()
+	return check.AdvanceTo(ckpt.index + Index(amount))
+}
+
+func (check Checkpoint[E, S]) AdvanceTo(index Index) error {
+	ckpt := check.data()
+	if !check.log.HasEntryAt(index) || index == check.log.IndexBeforeFirst() {
+		return fmt.Errorf("cannot advance checkpoint by requested amount, that would send us past the end of the log!")
+	}
+	if index < ckpt.index {
+		return fmt.Errorf("cannot advance checkpoint by negative amount! requested advance to index %v but we were already at index %v.", index, ckpt.index)
+	}
+	ckpt.index = index
+	return ckpt.updateSnapshot(check.log)
+}
+
+func (check Checkpoint[E, S]) Close() {
+	delete(check.log.checkpoints, check.id)
 }
